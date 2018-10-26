c0556f4a5da8afb907ae61225eb8a449ff9e7e5f
Evelyn <student_104@ool-ad0202f5.dyn.optonline.net>
2018-10-26 11:29:44 -0400

    added event listeners to piece class

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const board = document.querySelector('.board');
-
+// const board = document.querySelector('.board');
+// const playPieces = document.querySelectorAll('.piece');
+// console.log(playPieces);
+const board = Array.from({length: 4}, ()=> Array.from({length: 4}, () => null))
 
 //MAKE THE GAME BOARD
 
 function renderGameBoard(){
   for (let i = 0; i < 16; i += 1){
     let gameBoard = document.createElement('div');
-    board.appendChild(gameBoard);
+    // board.appendChild(gameBoard);
     gameBoard.setAttribute('class', 'play');
   }
 }
@@ -17,7 +19,7 @@ renderGameBoard();
 
 function makePieces() {
   const size = ['large', 'small'];
-  const color = ['light wood', 'dark wood'];
+  const color = ['light', 'dark'];
   const shape = ['round', 'square'];
   const top = ['flat', 'hollow'];
   const pieces = [];
@@ -25,12 +27,13 @@ function makePieces() {
   for (let i = 0; i < size.length; i += 1){
     for (let j = 0; j < color.length; j += 1){
       for (let k = 0; k < shape.length; k += 1){
-        for (let l = 0; l < top.length; l +=1){
+        for (let l = 0; l < top.length; l += 1){
           const piece = {
             size: size[i],
             color: color[j],
             shape: shape[k],
-            top: top[l]
+            top: top[l],
+            id: `piece-${pieces.length}`
           }
           pieces.push(piece)
         }
@@ -39,4 +42,42 @@ function makePieces() {
   }
   return pieces;
 }
-console.log(makePieces());
+// console.log(makePieces());
+
+function makePiecesHTML() {
+  for (let i = 0; i < pieces.length; i++) {
+    const piece = pieces[i];
+    const pieceEl = document.createElement('div');
+    pieceEl.className = `piece ${piece.size} ${piece.color} ${piece.shape} ${piece.top}`;
+    pieceEl.id = `${piece.id}`;
+    pieceEl.dataset.index = i;
+    pieceEl.addEventListener("click", function(e) {
+      e = document.querySelectorAll('.piece')
+      console.log('You clicked me!');
+      function selectPiece(i){
+        if (selected === piece[i]) {
+          //deselect
+        } else {
+          //selected
+          selected === piece [i]
+        }
+      }
+     });
+    document.body.appendChild(pieceEl);
+  }
+}
+
+let pieces = makePieces();
+makePiecesHTML();
+
+function positionSelect(row, column) {
+  if (selected) {
+    //check if move is valid
+    //if so, do something
+    //if else, do nothing or return an error
+  } else {
+    //let user know a piece needs to be selected
+  }
+}
+
+//win condition
